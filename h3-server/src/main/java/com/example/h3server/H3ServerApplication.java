5f8d398f3d07a732880a1b80c6bc45cab92a26e6
Nikita Koynov <nikicha03@gmail.com>
2020-08-31 20:12:25 +0300

    server app

---
+++ h3-server/src/main/java/com/example/h3server/H3ServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.h3server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H3ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H3ServerApplication.class, args);
+	}
+
+}
---
+++ h3-server/src/test/java/com/example/h3server/H3ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.h3server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H3ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
