24c9146bd9d70db2ddd3e14abd5237547474b2fe
Nikita Koynov <nikicha03@gmail.com>
2020-09-14 15:46:31 +0300

    setting cors mappings

--- h3-server/src/main/java/com/example/h3server/configuration/WebConfiguration.java
+++ h3-server/src/main/java/com/example/h3server/configuration/WebConfiguration.java
@@ -1,4 +1,16 @@
 package com.example.h3server.configuration;
 
-public class WebConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        // TODO change origins when deploying
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+    }
 }
