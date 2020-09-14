2184c655512f3e231efdaaf45a5d1ebcb75fff38
Nikita Koynov <nikicha03@gmail.com>
2020-09-14 23:42:01 +0300

    better email validation

--- h3-server/src/main/java/com/example/h3server/models/User.java
+++ h3-server/src/main/java/com/example/h3server/models/User.java
@@ -30,7 +30,7 @@ public class User {
     private String username;
 
     @NotNull(message = "Email must be valid and from 3 to 225 symbols")
-    @Pattern(regexp = "^(.+)@(.+)$", message = "Email must be valid and from 3 to 225 symbols")
+    @Pattern(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])", message = "Email must be valid and from 3 to 225 symbols")
     @Size(min = 3, max = 225, message = "Email must be valid and from 3 to 225 symbols")
     @Column(unique = false, nullable = false)
     private String email;
