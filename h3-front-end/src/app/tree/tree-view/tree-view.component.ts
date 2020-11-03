5fddd106c5a6fb784ddd7b4e6ac52f6149fae645
Nikita Koynov <nikicha03@gmail.com>
2020-11-03 13:51:09 +0200

    not owning empty tree

--- h3-front-end/src/app/tree/tree-view/tree-view.component.ts
+++ h3-front-end/src/app/tree/tree-view/tree-view.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {map, switchMap, tap} from 'rxjs/operators';
+import {Subscription} from 'rxjs';
+import {ActivatedRoute} from '@angular/router';
+import {TreeService} from '../tree.service';
+import {AuthService} from '../../authentication/auth.service';
 
 @Component({
   selector: 'app-tree-view',
   templateUrl: './tree-view.component.html',
   styleUrls: ['./tree-view.component.css']
 })
-export class TreeViewComponent implements OnInit {
+export class TreeViewComponent implements OnInit, OnDestroy {
+  isOwner: boolean;
+  private userSub: Subscription;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private treeService: TreeService,
+              private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.userSub = this.route.url.pipe(
+      map(urlSegment => +urlSegment[0]),
+      switchMap(treeId => {
+        return this.treeService.getTree(treeId).pipe(
+          switchMap(tree => {
+            return this.authService.user.pipe(
+              map(user => user.username === tree.owner)
+            );
+          }));
+      })
+    ).subscribe(isOwner => {
+      this.isOwner = isOwner;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
   }
 
 }
