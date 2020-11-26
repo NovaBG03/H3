9f8be14d3e92f07887881801a13d4d54b3eb6b6e
Nikita Koynov <nikicha03@gmail.com>
2020-11-26 10:15:57 +0200

    fix new tree bug

--- h3-server/src/main/java/com/example/h3server/mappers/FamilyTreeMapper.java
+++ h3-server/src/main/java/com/example/h3server/mappers/FamilyTreeMapper.java
@@ -5,6 +5,7 @@ import com.example.h3server.dtos.tree.FamilyTreeResponseDTO;
 import com.example.h3server.models.FamilyTree;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -14,10 +15,10 @@ public interface FamilyTreeMapper {
 
     @Mapping(target = "owner", source = "user.username")
     @Mapping(target = "tags",
-            expression = "java(familyTree.getTags().stream().map(treeTag -> treeTag.getLabel()).collect(java.util.stream.Collectors.toList()))")
+            expression = "java(familyTree.getTags() == null ? com.google.common.collect.Lists.newArrayList() : familyTree.getTags().stream().map(treeTag -> treeTag.getLabel()).collect(java.util.stream.Collectors.toList()))")
     FamilyTreeResponseDTO familyTreeToFamilyTreeResponseDTO(FamilyTree familyTree);
 
     @Mapping(target = "tags",
-            expression = "java(familyTreeDataDTO.getTags().stream().map(tag -> com.example.h3server.models.TreeTag.builder().label(tag).build()).collect(java.util.stream.Collectors.toSet()))")
+            expression = "java(familyTreeDataDTO.getTags() == null ? com.google.common.collect.Sets.newHashSet() : familyTreeDataDTO.getTags().stream().map(tag -> com.example.h3server.models.TreeTag.builder().label(tag).build()).collect(java.util.stream.Collectors.toSet()))")
     FamilyTree familyTreeDataDTOToFamilyTree(FamilyTreeDataDTO familyTreeDataDTO);
 }
